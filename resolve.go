9ce4e6a080d5c33599f3e8fbc8fba194d306cdbc
Sladeck <gm.moulin@hotmail.fr>
2017-12-11 12:43:23 +0100

    [ADD] initial commit

---
+++ resolve.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "log"
+    "strings"
+    "strconv"
+	"sync"
+)
+
+type Sudoku struct {
+  Grid[9][9]int
+}
+
+func (this Sudoku) ReadFile() []Sudoku{
+  const path = "./Sudoku/";
+  var arrayReturn []Sudoku
+
+  files, err := ioutil.ReadDir(path)
+  if err != nil {
+      log.Fatal(err)
+  }
+
+  for _, f := range files {
+          fmt.Println(f.Name())
+          b, err := ioutil.ReadFile(path + f.Name()) // just pass the file name
+          if err != nil {
+              fmt.Print(err)
+          }
+
+          str := string(b) // convert content to a 'string'
+          var valuesInFiles = strings.Split(str, "\r\n")
+
+          for index, _ := range valuesInFiles {
+              var valuesInArray = strings.Split(valuesInFiles[index], "")
+              for o, _ := range valuesInArray {
+                  var value = 0
+                  if(valuesInArray[o] == ".") {
+                    value = 0
+                  } else {
+                    v, _ := strconv.Atoi(valuesInArray[o])
+                    value = v
+                  }
+                  this.Grid[index][o] = value
+              }
+
+          }
+          fmt.Println(this)
+          arrayReturn = append(arrayReturn, this)
+  }
+  return arrayReturn
+
+}
+
+
+func (this *Sudoku) Display() {
+	for kx, x := range this.Grid {
+		for ky, y := range x {
+			fmt.Print(y)
+			if ky == 2 || ky == 5 {
+				fmt.Print("|")
+			}
+		}
+		fmt.Println("")
+		if kx == 2 || kx == 5 {
+			for i := 0; i < 11; i++ {
+				fmt.Print("-")
+			}
+			fmt.Println("")
+		}
+	}
+}
+
+func (this *Sudoku) Check() bool {
+	for x := 0; x < 9; x++ {
+		acc := make(map[int]bool)
+		for y := 0; y < 9; y++ {
+			val := this.Grid[x][y]
+			if acc[val] && val != 0 {
+				return false
+			}
+			acc[val] = true
+		}
+	}
+
+	for y := 0; y < 9; y++ {
+		acc := make(map[int]bool)
+		for x := 0; x < 9; x++ {
+			val := this.Grid[x][y]
+			if acc[val] && val != 0 {
+				return false
+			}
+			acc[val] = true
+		}
+	}
+
+	for cadX := 0 ; cadX < 3 ; cadX++ {
+	    for cadY := 0 ; cadY < 3 ; cadY++ {
+	        acc := make(map[int]bool)
+
+	        for x := cadX*3; x < 3; x++ {
+	            for y := cadY*3; y < 3; y++ {
+    	            val := this.Grid[x][y]
+        			if acc[val] && val != 0 {
+        				return false
+        			}
+        			acc[val] = true
+    	        }
+	        }
+	    }
+	}
+
+	return true
+}
+
+func (this *Sudoku) Solve() {
+	coords := this.getMissingsNumbers()
+	fmt.Println(this.solveRecursion(coords))
+}
+
+func (this *Sudoku) getMissingsNumbers() (res [][2]int) {
+	for ky, vy := range this.Grid {
+		for kx, vx := range vy {
+			if vx == 0 {
+				add := [2]int{ky, kx}
+				res = append(res, add)
+			}
+		}
+	}
+	fmt.Println(res)
+	return
+}
+
+func (this *Sudoku) solveRecursion(coords [][2]int) bool {
+	if len(coords) == 0 {
+		return true
+	}
+	y := coords[0][0]
+	x := coords[0][1]
+	for n := 1; n <= 9; n++ {
+		//fmt.Println("testing:", y, x, n)
+		if this.checkCoord(y, x, n) {
+			//fmt.Println("testing YES:", y, x, n)
+			this.Grid[y][x] = n
+			if this.solveRecursion(coords[1:]) {
+				return true
+			}
+			this.Grid[y][x] = 0
+		}
+	}
+	return false
+}
+
+func (this *Sudoku) checkCoord(cy int, cx int, nVal int) bool {
+	// Line
+	for x := 0 ; x < 9 ; x++ {
+		val := this.Grid[cy][x]
+		if val == nVal {
+			return false
+		}
+	}
+
+	// Col
+	for y := 0 ; y < 9 ; y++ {
+		val := this.Grid[y][cx]
+		if val == nVal {
+			return false
+		}
+	}
+
+	// square
+	by := cy - (cy % 3)
+	bx := cx - (cx % 3)
+	for y := 0; y < 3; y++ {
+		for x := 0; x < 3; x++ {
+
+			val := this.Grid[by + y][bx + x]
+			if val == nVal {
+				return false
+			}
+
+		}
+
+	}
+
+	return true
+}
+
+func solver(ch chan Sudoku, wg *sync.WaitGroup) {
+	for {
+		var val Sudoku
+		val = <- ch // Receive
+		fmt.Println()
+		val.Solve()
+		val.Display()
+		fmt.Println()
+		wg.Done()
+	}
+}
+
+func main() {
+	var base Sudoku
+	var ch chan Sudoku
+    ch = make(chan Sudoku)
+    var wg sync.WaitGroup
+
+	var table = base.ReadFile()
+
+	go solver(ch, &wg) // renvoyer un deuxième channel pour un seul Display (par rapport au solver)
+	go solver(ch, &wg)
+	go solver(ch, &wg)
+	go solver(ch, &wg)
+
+	for _, val := range table {
+		wg.Add(1)
+		ch <- val
+	}
+	wg.Wait()
+}
